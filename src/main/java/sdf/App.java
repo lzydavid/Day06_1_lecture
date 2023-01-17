5f0f0df9d9c5e92885493947c3b7e4a07d65780e
lzydavid <nodotanolife@hotmail.com>
2023-01-17 11:02:50 +0800

    Basic threading example

--- src/main/java/sdf/App.java
+++ src/main/java/sdf/App.java
@@ -4,6 +4,7 @@ package sdf;
  * Hello world!
  */
 public final class App {
+    
     private App() {
     }
 
@@ -12,6 +13,23 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Thread thread1 = new Thread(new Runnable(){
+            
+            @Override
+            public void run(){
+                for (int i=0;i<5;i++){
+                    System.out.println(Thread.currentThread().getName()+ "\tRunnable ..."+i); 
+                }
+            }
+        });
+        thread1.start();
+
+        MyRunnableImplementation mRI = new MyRunnableImplementation();
+        Thread thread2 = new Thread(mRI);
+        thread2.run();
+
+        Thread thread3 = new Thread(mRI);
+        thread3.run();
     }
 }
---
+++ src/main/java/sdf/MyRunnableImplementation.java
@@ -0,0 +1,23 @@
+package sdf;
+public class MyRunnableImplementation implements Runnable {
+
+    private String taskName;
+
+    public MyRunnableImplementation(){
+    }
+
+    public MyRunnableImplementation(String taskName){
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        
+        for(int i=0;i<5;i++){
+            System.out.println(Thread.currentThread().getName()+ "\tRunnable ..."+i);
+        }
+
+        
+    }
+    
+}
